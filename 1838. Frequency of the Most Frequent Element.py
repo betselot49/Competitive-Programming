9a808ab70b9eb2dd3b89e4fd9e6829b40457d909
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-08-29 03:58:56 -0700

    Create 1838. Frequency of the Most Frequent Element.py

---
+++ 1838. Frequency of the Most Frequent Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left, right = 0, 0
+        total, maximum = 0, 0
+        while right < len(nums):
+            total += nums[right]
+            while nums[right] * (right-left+1) > total + k:
+                total -= nums[left]
+                left += 1
+            maximum = max(maximum, right-left+1)
+            right += 1
+        return maximum
