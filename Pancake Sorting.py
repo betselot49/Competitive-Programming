d2ea081184f8b6121aa855c67949bd954b494c27
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-08-30 06:20:31 -0700

    Create Pancake Sorting.py

---
+++ Pancake Sorting.py
@@ -0,0 +1,16 @@
+class Solution:
+    def flip(self, l, r):
+        l.reverse()
+        return l + r
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        flips = []
+        for i in range(n, 0, -1):
+            for j in range(0, i):
+                if arr[j] == i:
+                    arr = self.flip(arr[:j+1], arr[j+1:])
+                    flips.append(j+1)
+                    break
+            arr = self.flip(arr[:i], arr[i:])
+            flips.append(i)
+        return flips
