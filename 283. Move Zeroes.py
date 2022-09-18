380a653d0582ffdaa353108b0be30a3a4f17be3a
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-09-18 03:16:35 -0700

    Create 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = -1
+        j = 0
+        while j < len(nums):
+            if nums[j] == 0 and i == -1:
+                i = j
+            elif nums[j] != 0 and i != -1:
+                nums[j], nums[i] = nums[i], nums[j]
+                i += 1
+            j += 1  
