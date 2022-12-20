e80e779e1c4e9e7d43d585f2fcb10a91c995cdeb
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-12-20 19:52:46 +0300

    Time: 174 ms (56.78%), Space: 13.9 MB (73.32%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,5 +1,9 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
+        def fact(num):
+            for j in range(num-1, 1, -1):
+                num *= j
+            return num
         list1 = []
         for word in words:
             Set = set(word)
@@ -8,13 +12,23 @@ class Solution:
             sort = ''.join(sort)
             list1.append(sort)
             
-        i = 0
-        counter = 0
-        while i < len(list1):
-            j = i + 1
-            while j < len(list1):
-                if list1[i] == list1[j]:
-                    counter += 1
-                j += 1
-            i += 1
-        return counter
+#         print(list1)
+#         i = 0
+#         counter = 0
+#         while i < len(list1):
+#             j = i + 1
+#             while j < len(list1):
+#                 if list1[i] == list1[j]:
+#                     counter += 1
+#                 j += 1
+#             i += 1
+#         return counter
+        c = 0
+        list1 = dict(Counter(list1))
+        for word in list1:
+            num = list1[word]
+            if num > 2:    
+                c += fact(num)/(fact(num-2)*2)  
+            elif num == 2:
+                c += 1
+        return int(c)
