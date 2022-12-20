f9bd3202dcf473b0f482c0984ef80a1843a99744
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-12-20 16:26:29 +0300

    Update Word order.py

--- Word order.py
+++ Word order.py
@@ -1,1 +1,16 @@
+from collections import defaultdict
 
+n = int(input())
+words = []
+for i in range(n):
+    x = input()
+    words.append(x)
+    
+hashM = defaultdict(lambda: 0)
+for word in words:
+    hashM[word] += 1
+    
+print(len(hashM))
+ans = ""
+for word in hashM:
+    print(hashM[word], end=" ")
