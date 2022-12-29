ff30bb09f0771871f9f0432d89c0d51de4196f85
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-12-29 18:29:40 +0300

    Time: 184 ms (98.87%), Space: 18 MB (27.08%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,54 +1,31 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        """
-           [1,  2,  3]
-              
-         
-           [4,  5,  6]
-          
-        
-     row = [7,  8,  9]
-            col
-          
-         
-         row = 0
-         col = 0
-         
-         ans = 1, 2, 4
-        
-        append, if row == 0 => col += 1 => append while row != 0 => <-------------------------|                         
-                                                                                              |
-        row += 1 ans col -= 1 then append, if col == 0 => row += 1 appen while row != 0 =>    |
-                                                                                              |
-        row -= 1 and col += 1 then appen, if row == 0 => col += 1 => append while  -----------|
-        
-        """
         
         row, col, ans = 0, 0, []
         ans.append(mat[row][col])
         
-        while row < len(mat) - 1 or col < len(mat[0]) - 1:
+        while row < len(mat) - 1 or col < len(mat[0]) - 1: # while there is space right and below
             
-            if col < len(mat[0]) - 1:
+            if col < len(mat[0]) - 1: # check space to right
                 col += 1
                 ans.append(mat[row][col])
-            elif row < len(mat) - 1:
+            elif row < len(mat) - 1: # check space below
                 row += 1
                 ans.append(mat[row][col])
         
-            while row < len(mat) - 1 and col > 0:
+            while row < len(mat) - 1 and col > 0:  # while there is space below and left
                 row += 1
                 col -= 1
                 ans.append(mat[row][col])
         
-            if row < len(mat) - 1:
+            if row < len(mat) - 1:  # check space below 
                 row += 1
                 ans.append(mat[row][col])
-            elif col < len(mat[0]) - 1:
+            elif col < len(mat[0]) - 1: # check space right
                 col += 1
                 ans.append(mat[row][col])
             
-            while col < len(mat[0]) - 1 and row > 0:
+            while col < len(mat[0]) - 1 and row > 0:  # while there is space above and right
                 col += 1
                 row -= 1
                 ans.append(mat[row][col])
