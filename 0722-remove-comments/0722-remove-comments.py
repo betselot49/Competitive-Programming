400ec7430ce1431284be4e956627e3867375605f
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2022-12-29 16:25:26 +0300

    Time: 36 ms (80.72%), Space: 13.9 MB (45.63%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -1,37 +1,11 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:       
-        """
-        ["     /*Test program */     ", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a
-                i
-        
-        test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"]
-        
-       
-       
-       
-       stack =  [' ', ' ', ' ', /, ]
-       
-       if /, check if next is / then move your pointer until you get #
-       if /, and next is * move your pointer until you find * and next /
-       if # and stack[-1] is not / push to answer
-       
-       iterate through the source,  
-       
-       """
-        
-        
-        
-        """
-joined = "     /*Test program       #int main()#{ #   variable  */ declaration #int a, b, c;#/* This is a test#   multiline  #   comment for #   testing */#a = b + c;#}
-"
-        """
     
         joined = "#".join(source)
         stack = []
         ans = []
         pointer = 0
-        print(joined)        
-
+        
         while pointer < len(joined):
             if joined[pointer] == "/":
                 if joined[pointer + 1] == "/":
