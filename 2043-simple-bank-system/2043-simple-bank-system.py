05a81497c6594ed239271237a631dd05a14ba489
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-02 11:17:52 +0300

    Time: 748 ms (72.12%), Space: 43.9 MB (46.84%) - LeetHub

---
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -0,0 +1,31 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.len = len(self.balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 <= self.len and account2 <= self.len and self.balance[account1-1] >= money:
+            self.balance[account2 - 1] += money
+            self.balance[account1 - 1] -= money
+            return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account <= self.len:
+            self.balance[account-1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account <= self.len and self.balance[account-1] >= money:
+            self.balance[account-1] -= money
+            return True
+        return False
+        
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
