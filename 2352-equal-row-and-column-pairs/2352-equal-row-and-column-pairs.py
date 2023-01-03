e5aac18229448d03b9a0be96350da3103d2d7c7b
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-03 17:32:18 +0300

    Time: 1768 ms (21.97%), Space: 19.2 MB (14.05%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:        
+        rowDict = defaultdict(int)
+        
+        for row in grid:  # add rows with their frequency in rowDict in their string form
+            key = ""
+            for column in row:
+                key += str(column) + "#"
+            rowDict[key] += 1
+            
+        count = 0
+        for column in range(len(grid[0])):  # count each column's match in rowDict 
+            key = ""
+            for row in grid:
+                key += str(row[column]) + "#"  # to scape cases like [[11,1],[1,11]]
+            count += rowDict[key]    
+        
+        return count
