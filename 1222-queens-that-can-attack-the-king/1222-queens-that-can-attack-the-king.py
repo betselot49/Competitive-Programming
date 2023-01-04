cdd0d21d6d5c4402e162dbcb1fda9264ddd0651d
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-04 20:51:07 +0300

    Time: 65 ms (64.52%), Space: 14 MB (17.05%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,18 +1,61 @@
 class Solution:
-    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:      
-        setQueens = {(point1, point2) for point1, point2 in queens}
-        attackers = []
-        case = [-1, 0, 1]
+        def minDistance(self, reference, point1, point2):
+            distance1 = ((reference[0] - point1[0]) ** 2 + (reference[1] - point1[1]) ** 2) ** 0.5
+            distance2 = ((reference[0] - point2[0]) ** 2 + (reference[1] - point2[1]) ** 2) ** 0.5
+            if distance1 < distance2:
+                return point1
+            return point2
+
+    
+        def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:      
+            setQueens = defaultdict(int)
+            possibleQueens = [0] * 8
+
+            setQueens = {(0,1): 1, (0,-1): 2, (1,0): 3, (-1,0): 4, (1,1): 5, (-1,-1): 6, (-1,1): 7, (1,-1): 8}
+
+            for queen in queens:
+                p1, p2 = queen[0] - king[0], queen[1] - king[1] 
+                diff = [p1, p2]
+                s1, s2 = 0, 0
+                if p1 == 0 or p2 == 0 or abs(p1) == abs(p2):
+                    if p1 != 0:
+                        s1 = p1 // abs(p1)
+                        s2 = p2 // abs(p1)
+                    else:
+                        s1 = p1 // abs(p2)
+                        s2 = p2 // abs(p2)
+
+                if (s1, s2) in setQueens:
+                    key = setQueens[(s1, s2)] - 1
+                    if possibleQueens[key] == 0:
+                        possibleQueens[key] = queen
+                    else:
+                        possibleQueens[key] = self.minDistance(king, queen, possibleQueens[key])
+
+            queensAtack = []
+            for queen in possibleQueens:
+                if queen:
+                    queensAtack.append(queen)
+
+            return queensAtack
         
-        for index1 in case:
-            for index2 in case:
-                for index3 in range(8):
-                    point1, point2 = king[0] + index1 * index3, king[1] + index2 * index3
-                    if (point1, point2) in setQueens:
-                        attackers.append([point1, point2])
-                        break
+                    
+
+        
+        
+#         setQueens = {(point1, point2) for point1, point2 in queens}
+#         attackers = []
+#         case = [-1, 0, 1]
+        
+#         for index1 in case:
+#             for index2 in case:
+#                 for index3 in range(8):
+#                     point1, point2 = king[0] + index1 * index3, king[1] + index2 * index3
+#                     if (point1, point2) in setQueens:
+#                         attackers.append([point1, point2])
+#                         break
                         
-        return attackers
+#         return attackers
     
     
 
@@ -76,12 +119,12 @@ class Solution:
             
 #         return queensAtack
 
-#         def minDistance(self, reference, point1, point2):
-#             distance1 = ((reference[0] - point1[0]) ** 2 + (reference[1] - point1[1]) ** 2) ** 0.5
-#             distance2 = ((reference[0] - point2[0]) ** 2 + (reference[1] - point2[1]) ** 2) ** 0.5
-#             if distance1 < distance2:
-#               return point1
-#             return point2
+        # def minDistance(self, reference, point1, point2):
+        #     distance1 = ((reference[0] - point1[0]) ** 2 + (reference[1] - point1[1]) ** 2) ** 0.5
+        #     distance2 = ((reference[0] - point2[0]) ** 2 + (reference[1] - point2[1]) ** 2) ** 0.5
+        #     if distance1 < distance2:
+        #       return point1
+        #     return point2
 
 
 
