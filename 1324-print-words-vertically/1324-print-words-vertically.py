ce3201c686d9b77599e6d8f59380cff770ec6239
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-04 17:13:20 +0300

    Time: 52 ms (57.30%), Space: 13.8 MB (64.33%) - LeetHub

--- 1324-print-words-vertically/1324-print-words-vertically.py
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -1,11 +1,11 @@
 class Solution:
     def printVertically(self, s: str) -> List[str]:
-        given = s.split(" ")
+        given = s.split(" ")  # O(N) = where N = 200
         ans, index = [], 0
 
-        while True:
+        while True:  
             space, temp = 0, ""
-            for word in given:
+            for word in given:  # O(N) = where N = 200
                 if index < len(word):
                     temp += word[index]
                     space = len(temp)
@@ -14,6 +14,6 @@ class Solution:
             if space == 0:
                 break
             else:
-                ans.append(temp[:space])
+                ans.append(temp[:space])  # O(M)  Where M = length of the maximim word
                 index += 1
         return ans
