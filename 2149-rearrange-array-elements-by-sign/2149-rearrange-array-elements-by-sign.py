631777e649d492ea0b2ea19aeee2ce6c98869ad6
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-05 19:48:59 +0300

    Time: 3473 ms (47.17%), Space: 45.8 MB (15.73%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,23 +1,41 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        negative, positive, rearranged = 0, 0, []
+        positive, negative, rearranged = [], [], []
+     
+        for num in nums:
+            if num < 0:
+                negative.append(num)
+            else:
+                positive.append(num)
+                
+        for index in range(len(negative)):
+            rearranged.append(positive[index])
+            rearranged.append(negative[index])
         
-        while positive < len(nums) or negative < len(nums):
-            while positive < len(nums):
-                if nums[positive] > 0:
-                    rearranged.append(nums[positive])
-                    positive += 1
-                    break
-                positive += 1
-                
-            while negative < len(nums):
-                if nums[negative] < 0:
-                    rearranged.append(nums[negative])
-                    negative += 1
-                    break
-                negative += 1
-                
         return rearranged
+    
+    
+
+#  < ==============  reduced space   =================== >    
+    
+#         negative, positive, rearranged = 0, 0, []
+        
+#         while positive < len(nums) or negative < len(nums):
+#             while positive < len(nums):
+#                 if nums[positive] > 0:
+#                     rearranged.append(nums[positive])
+#                     positive += 1
+#                     break
+#                 positive += 1
+                
+#             while negative < len(nums):
+#                 if nums[negative] < 0:
+#                     rearranged.append(nums[negative])
+#                     negative += 1
+#                     break
+#                 negative += 1
+                
+#         return rearranged
             
             
         
@@ -25,19 +43,6 @@ class Solution:
         
         
     
-#         positive, negative, rearranged = [], [], []
-     
-#         for num in nums:
-#             if num < 0:
-#                 negative.append(num)
-#             else:
-#                 positive.append(num)
-                
-#         for index in range(len(negative)):
-#             rearranged.append(positive[index])
-#             rearranged.append(negative[index])
-        
-#         return rearranged
     
 
     
