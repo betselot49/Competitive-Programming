274bd392d518091d4e0894efd6e507ee9f70f874
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-05 19:47:13 +0300

    Time: 3804 ms (37.26%), Space: 45.3 MB (38.55%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,16 +1,44 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positive = []
-        negative = []
-        rearranged = []
-        for num in nums:
-            if num < 0:
-                negative.append(num)
-            else:
-                positive.append(num)
-                
-        for index in range(len(negative)):
-            rearranged.append(positive[index])
-            rearranged.append(negative[index])
+        negative, positive, rearranged = 0, 0, []
         
+        while positive < len(nums) or negative < len(nums):
+            while positive < len(nums):
+                if nums[positive] > 0:
+                    rearranged.append(nums[positive])
+                    positive += 1
+                    break
+                positive += 1
+                
+            while negative < len(nums):
+                if nums[negative] < 0:
+                    rearranged.append(nums[negative])
+                    negative += 1
+                    break
+                negative += 1
+                
         return rearranged
+            
+            
+        
+        
+        
+        
+    
+#         positive, negative, rearranged = [], [], []
+     
+#         for num in nums:
+#             if num < 0:
+#                 negative.append(num)
+#             else:
+#                 positive.append(num)
+                
+#         for index in range(len(negative)):
+#             rearranged.append(positive[index])
+#             rearranged.append(negative[index])
+        
+#         return rearranged
+    
+
+    
+    
