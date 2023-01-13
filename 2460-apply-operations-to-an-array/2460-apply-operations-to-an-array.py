4620a922839cd923c919cc495865bdffcdbe8ab7
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-13 18:02:00 +0300

    Time: 54 ms (87.75%), Space: 14.2 MB (60.39%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -1,22 +1,37 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i+1]:
-                nums[i] *= 2
-                nums[i+1] = 0
+#         for index in range(len(nums) - 1):
+#             if nums[index] == nums[index+1]:
+#                 nums[index] *= 2
+#                 nums[index+1] = 0
                 
+            
                 
-        left, right = 0, 0
+#         left, right = 0, 0
         
-        while right < len(nums):
-            while left < len(nums) and nums[left] != 0:
+#         while right < len(nums):
+#             while left < len(nums) and nums[left] != 0:
+#                 left += 1
+                
+#             right = left
+#             while right < len(nums) and nums[right] == 0:
+#                 right += 1
+                
+#             if right < len(nums):
+#                 nums[left], nums[right] = nums[right], nums[left]     
+                
+#         return nums
+    
+    # ===============  more optimal solution =================
+        
+        left = 0
+        for index in range(len(nums)):
+            if index + 1 < len(nums) and nums[index] == nums[index+1]:
+                nums[index] *= 2
+                nums[index+1] = 0
+               
+            if nums[index]:
+                nums[index], nums[left] = nums[left], nums[index]
                 left += 1
-                
-            right = left
-            while right < len(nums) and nums[right] == 0:
-                right += 1
-                
-            if right < len(nums):
-                nums[left], nums[right] = nums[right], nums[left]     
                 
         return nums
