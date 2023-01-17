9bed95cdc6e5da45d7268b6f02f3f24f572fdaf1
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-17 19:20:13 +0300

    Time: 611 ms (10.25%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,26 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        j = len(heights) - 1
-        while j > 0:
-            i = 0
-            while i < j:
-                if heights[i] < heights[i+1]:
-                    names[i], names[i+1] = names[i+1], names[i]
-                    heights[i], heights[i+1] = heights[i+1], heights[i]
-                i += 1
-            j -= 1
+  #  < ======  using buble sort ======> 
+
+
+#         for j in range(len(names)-1, 0, -1)
+#            for i in range(j):
+#                 if heights[i] < heights[i+1]:
+#                     names[i], names[i+1] = names[i+1], names[i]
+#                     heights[i], heights[i+1] = heights[i+1], heights[i]            
+#         return names
+
+
+#  < ==========   using Selection Sort  ========> 
+    
+        for i in range(len(names)):
+            curMax = i
+            for j in range(i, len(names)):
+                if heights[j] > heights[curMax]:
+                    curMax = j
+                    
+            heights[i], heights[curMax] =  heights[curMax], heights[i] 
+            names[i], names[curMax] =  names[curMax], names[i]
             
         return names
             
