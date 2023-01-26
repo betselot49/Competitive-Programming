a4801660a03f06e2cd1b454e4982fb83db13021a
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-01-26 18:24:22 +0300

    Time: 186 ms (5.06%), Space: 14 MB (15.27%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        partition = [[s[0]]]
+        counter, j = [1], 1
+      
+        while j < len(s):
+            i = 0
+            while i < len(partition) and s[j] not in partition[i]:
+                i += 1
+            if i == len(partition):
+                partition.append([s[j]])
+                counter.append(1)
+            else:
+                while len(partition) > i+1:
+                    partition[i] += partition.pop()
+                    counter[i] += counter.pop()
+                partition[i] += [s[j]]
+                counter[i] += 1
+            j += 1
+        return counter
+            
+        
+                       
+            
