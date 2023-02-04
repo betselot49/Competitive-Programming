8c1bd5c7e179cec71316c408e48a0efe8ddaa812
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-02-04 23:36:19 +0300

    Time: 98 ms (58.45%), Space: 13.9 MB (64.59%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,42 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter = Counter(s1)
+        k = len(s1)
+        i = 0
+      
+        
+        while i < len(s2) and s2[i] not in counter:
+            i += 1
+          
+        j = i
+        
+        while i < len(s2):
+            if i >= j:
+                j = i
+            while j < len(s2) and counter[s2[j]] > 0:
+                counter[s2[j]] -= 1
+                j += 1
+                if j - i == k:
+                    return True
+            if s2[i] in counter:
+                counter[s2[i]] += 1
+            i += 1
+            
+            while i < len(s2) and s2[i] not in counter:
+                i += 1
+
+        return False
+                
+        
+        """
+        
+        "ky"
+        
+        "ainwkckifykxlribaypk"
+              |
+              |
+        
+        """
+                    
+
+            
