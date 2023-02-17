b1bf87177016047ffc217190020bdc7142680893
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-02-17 22:46:06 +0300

    Update 0086-partition-list.py

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -8,20 +8,20 @@ class Solution:
         runner = ListNode(0)
         runner.next = head  
         head = runner
-        while runner.next and runner.next.val < x:
+        while runner.next and runner.next.val < x:  # run util you reach a node that its next node has less value than x.
             runner = runner.next
             
         collector = runner
         while collector.next:
             if collector.next.val < x:
-                temp1 = collector.next
-                collector.next = collector.next.next
-                temp2 = runner.next
+                temp1 = collector.next      #  Going to be the next of runner
+                collector.next = collector.next.next   
+                temp2 = runner.next         # Going to be the next of next of runner
                 runner.next = temp1
                 runner = runner.next
                 runner.next = temp2
             else:
-                collector = collector.next
+                collector = collector.next   
                 
                 
         return head.next
