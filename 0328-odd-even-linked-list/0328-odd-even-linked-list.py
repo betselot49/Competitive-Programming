54136e06bd146af031246b7977afe4e16c8ed3ca
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-02-20 17:46:20 +0300

    Time: 49 ms (46.03%), Space: 16.5 MB (68.11%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -3,45 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:        
-#         if head and head.next:
-#             dummy = head
-#             runner = head.next
-            
-#         while runner and runner.next:
-#             tempR = runner.next
-#             tempR.next = None
-#             runner.next = runner.next.next
-#             tempD = dummy.next
-#             dummy.next= tempR
-#             dummy = dummy.next
-#             dummy.next = tempD
-
-#             runner = runner.next
-            
-#         return head
-
-
 class Solution:
-    def oddEvenList(self, head: ListNode) -> ListNode:
-        oddHead = ListNode(0)
-        evenHead = ListNode(0)
-        odd = oddHead
-        even = evenHead
-        isOdd = True
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:      
+        if head and head.next:
+            even = head
+            odd = head.next
+            
+            while odd and odd.next:
+                tempOdd = odd.next
+                odd.next = odd.next.next
 
-        while head:
-            if isOdd:
-                odd.next = head
-                odd = head
-            else:
-                even.next = head
-                even = head
-            head = head.next
-            isOdd = not isOdd
+                tempEven = even.next
+                even.next = tempOdd
+                even = even.next
+                even.next = tempEven
 
-        even.next = None
-        odd.next = evenHead.next
-        return oddHead.next
-
+                odd = odd.next
+        return head
+        
+        
+        
+        
