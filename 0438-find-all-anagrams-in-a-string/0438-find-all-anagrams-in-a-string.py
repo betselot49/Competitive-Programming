7f13cf6fce1658bc94f323c95e207f1cccd67217
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-02-22 11:17:35 +0300

    Time: 153 ms (55.94%), Space: 15.1 MB (74.27%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,44 +1,21 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        count = Counter(p)
+        left = 0
+        ans = []
+        compare = defaultdict(int)
+        for right, num in enumerate(s):
+            compare[s[right]] += 1
+            if right - left + 1 == len(p):
+                if compare == count:
+                    ans.append(left)
+                compare[s[left]] -= 1
+                if compare[s[left]] == 0:
+                    del compare[s[left]]
+                left += 1
+            
+        return ans
+                
         
-        i, j, ans = 0, 0, [] 
-        c = Counter(p)
-        
-        while j < len(s):
-            if c.get(s[j], 0) > 0:
-                var = {s[j]: -1}
-                c.update(var)
-                j += 1
-                if j - i == len(p):
-                    ans.append(i)
-                    var = {s[i]: +1}
-                    c.update(var)
-                    i += 1
-            else:
-                while s[i] != s[j]:
-                    var = {s[i]: +1}
-                    c.update(var)
-                    i += 1
-                j += 1
-                i += 1
-
-        return(ans)
-     
-        
-        """
-        ------- brute force sollution -------------------   
-   
-        i, j, res, sortedp = 0, len(p) - 1, [], sorted(p)
-        
-        while j < len(s):
-            if sorted(s[i:j+1]) == sortedp:
-                res.append(i)
-            i += 1
-            j += 1
-        return res
-        
-        """
-        
-        
-    
-    
+            
+                
