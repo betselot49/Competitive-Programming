aaa01a0e59bc3d435131118a1b46ddc7981f3b96
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-02-22 10:52:22 +0300

    Time: 2650 ms (14.48%), Space: 32.2 MB (73.76%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        if k == 0:
+            return nums
+        cur_sum = 0
+        left = 0
+        res = [-1] * len(nums)
+        for right, num in enumerate(nums):
+            cur_sum += num
+            if right - left == 2 * k:
+                res[left + k] = cur_sum // (2 * k + 1)
+                cur_sum -= nums[left]
+                left += 1
+                
+        return res
