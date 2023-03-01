6b379142295f466ef7f346f489e45d4b62b3a48b
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-01 10:23:35 +0300

    Update 0021-merge-two-sorted-lists.py

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def merger(self, merged, node1, node2):
+        # Merge using recursion.
         if node1 == None or node2 == None:
             merged.next = node1 if node1 else node2
             return 
@@ -26,3 +27,21 @@ class Solution:
         
         self.merger(merged, list1, list2)
         return newNode.next
+    
+    
+    """
+    # Using iteration
+        merged = ListNode()
+        cur = merged
+        while list1 or list2:
+            val1 = list1.val if list1 else 101
+            val2 = list2.val if list2 else 101
+            cur.next = ListNode(min(val1, val2))
+            if val1 > val2:
+                list2 = list2.next
+            else:
+                list1 = list1.next
+            cur = cur.next
+        
+        return merged.next
+    """
