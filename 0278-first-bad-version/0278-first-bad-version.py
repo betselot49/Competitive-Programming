a1bcba29588d706386da2ae55d29f41a58cdb03d
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-03 16:23:00 +0300

    Time: 36 ms (36.21%), Space: 13.9 MB (50.70%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,12 +3,24 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        r = n-1
-        l = 0
-        while(l<=r):
-            mid = l + (r-l)//2
-            if isBadVersion(mid)==False:
-                l = mid+1
+#         r = n-1
+#         l = 0
+#         while(l<=r):
+#             mid = l + (r-l)//2
+#             if isBadVersion(mid)==False:
+#                 l = mid+1
+#             else:
+#                 r = mid-1
+#         return l
+    
+    
+    
+        low = 0
+        high = n+1
+        while low + 1 < high:
+            mid = low + (high - low) // 2
+            if isBadVersion(mid):
+                high = mid
             else:
-                r = mid-1
-        return l
+                low = mid
+        return high
