69e1f6999b540f21179aa96126276bc2defa0b52
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-05 23:23:01 +0300

    Time: 463 ms (84.32%), Space: 18.9 MB (57.58%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -5,22 +5,14 @@ class Solution:
         min_sum = 0
         
         for num in arr:  
-            count = 0
-            
-            # maintain the stack in increasing order
-            while inc_stack and inc_stack[-1][0] > num:
+            count = 1
+            while inc_stack and inc_stack[-1][0] >= num:
                 pop = inc_stack.pop()
                 total -= (pop[0] * pop[1])
                 count += pop[1]
-                
-            count += 1
-            if inc_stack and inc_stack[-1][0] == num:
-                inc_stack[-1][1] += count
-                total += (num * count)
-            else:
-                inc_stack.append([num, count])
-                total += (num * count)
-        
+            
+            inc_stack.append([num, count])
+            total += (num * count)
             min_sum += total
             
         return min_sum % (10 ** 9 + 7)
