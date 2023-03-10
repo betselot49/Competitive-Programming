ce3409b566833b1c9eb140f9c2abb60ce9658ad6
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-10 16:37:08 +0300

    Time: 53 ms (30.25%), Space: 20 MB (5.34%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -4,23 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         row_dict = defaultdict(list)
         def maxWidth(node, row, col):
-            if node == None:
+            if not node:
                 return
             
+            # putting col in dictionary using row as a key.
             if row_dict[row] and row_dict[row][1] < col:
                 row_dict[row][1] = col
             else:
                 row_dict[row] = [col, col]
                 
+            # do the same thing for left and right nodes.
             maxWidth(node.left, row+1, (col*2)-1)
             maxWidth(node.right, row+1, col*2)
             return 
         
-        maxWidth(root, 1, 1)
+        maxWidth(root, 1, 1)  # one indexed column.
+        
         width = 0
         for start, end in row_dict.values():
             width = max(width, end - start + 1)
