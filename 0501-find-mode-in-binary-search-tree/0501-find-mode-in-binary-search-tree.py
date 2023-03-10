0253f6445de83ce4beb49ddde3cc4f6fbe13a624
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-10 17:05:48 +0300

    Time: 59 ms (57.15%), Space: 18.5 MB (36.51%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def __init__(self):
         self.max = 0
@@ -12,15 +13,18 @@ class Solution:
     def mode(self, node):
         if not node: return 
 
+        # put the node.val and its count in the global dictionary and update the maximum count.
         self.mode_dict[node.val] += 1
         self.max = max(self.max, self.mode_dict[node.val])
 
+        # do the same thing for both left and right nodes.
         self.mode(node.left)
         self.mode(node.right)
         
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         self.mode(root)
         ans = []
+        # collect all numbers with only value equal to max_count into ans 
         for num in self.mode_dict:
             if self.mode_dict[num] == self.max:
                 ans.append(num)
