a5847b8ac216f28dc4d7fc741dfcbc00936b628c
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-14 20:27:45 +0300

    Time: 45 ms (7.66%), Space: 14 MB (17.88%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        array = []
+        def additive(ind):
+            print(array)
+            if ind >= len(num):
+                return len(array) >= 3
+            
+            for i in range(ind, len(num)):
+                curr = num[ind: i+1]
+                if (len(str(int(curr))) == len(curr) and   # checks for leading zeros except for the zero number it self alone
+                   (len(array) < 2 or array[-1] + array[-2] == int(curr))):  # cheks the sum of last two numbers == curr 
+                    array.append(int(curr))
+                    
+                    if additive(i+1):
+                        return True
+                    
+                    array.pop()
+            return False
+        
+        return additive(0)
