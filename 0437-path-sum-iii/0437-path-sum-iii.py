ea7e074ce9a24d69a4c721b7a00485fb7e4e32c9
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-21 18:21:17 +0300

    Time: 56 ms (68.78%), Space: 15.5 MB (31.66%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.seen = defaultdict(int)
-        self.seen[0] += 1
-        self.sum_count = 0
+        seen = defaultdict(int)
+        seen[0] += 1
+        sum_count = 0
         def recursion(node, pre_sum, targetSum):
+            nonlocal sum_count
             if not node: return 
             pre_sum += node.val
-            self.sum_count += self.seen[pre_sum - targetSum]
-            self.seen[pre_sum] += 1
+            sum_count += seen[pre_sum - targetSum]
+            seen[pre_sum] += 1
             recursion(node.left, pre_sum, targetSum)
             recursion(node.right, pre_sum, targetSum)
-            self.seen[pre_sum] -= 1
+            seen[pre_sum] -= 1
             
         recursion(root, 0, targetSum)
-        return self.sum_count
+        return sum_count
