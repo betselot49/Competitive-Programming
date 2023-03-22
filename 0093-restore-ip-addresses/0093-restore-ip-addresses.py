5736f41cc5d837a5eaf91ca2c9be90e022f50cf7
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-22 21:44:04 +0300

    Time: 45 ms (35.61%), Space: 13.8 MB (98.81%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,18 +1,18 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        self.valid = []
-        def backtrack(path, idx):
-            if len(path) == 4:
-                if idx == len(s):
-                    self.valid.append(".".join(path))
+        self.valid_ip = []
+        def backtrack(ip_address, idx):
+            if len(ip_address) == 4:  # If our ip_address collected enough ips: we don't need any more
+                if idx == len(s):  # If we got valid Ips, we append it self.valid_ip
+                    self.valid_ip.append(".".join(ip_address))
                 return 
                 
-            for i in range(idx, len(s)):
-                curr = s[idx:i+1]
-                if len(curr) == len(str(int(curr))) and 0 <= int(curr) <= 255:
-                    path.append(curr)
-                    backtrack(path[:], i+1)
-                    path.pop()
+            for i in range(idx, len(s)):  
+                curr = s[idx:i+1]  
+                if len(curr) == len(str(int(curr))) and 0 <= int(curr) <= 255:  # if current is valid element of ip address, add to ip_address.
+                    ip_address.append(curr)
+                    backtrack(ip_address[:], i+1)
+                    ip_address.pop()
                 
         backtrack([], 0)
-        return self.valid
+        return self.valid_ip
