7a2bad1031983e2eab05b261ca6473286012a0e0
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-23 19:40:32 +0300

    Time: 180 ms (24.34%), Space: 15.1 MB (98.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,13 +4,13 @@ class Solution:
         while idx < len(array):
             if array[idx] == -1: return idx
             
-            curr = array[idx]    
-            if array[idx] == len(array) or array[curr] == -1:
-                array[idx], array[curr-1] = array[curr-1], -1
-            elif idx != array[idx]:
-                array[curr], array[idx] = array[idx], array[curr]
-            
             if idx == array[idx]:
                 idx += 1
+            else:
+                curr = array[idx]    
+                if array[idx] == len(array) or array[curr] == -1:
+                    array[idx], array[curr-1] = array[curr-1], -1
+                elif idx != array[idx]:
+                    array[curr], array[idx] = array[idx], array[curr]
         
         return idx
