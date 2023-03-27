fbd96c6ed2f63adc8964aea300af37541d2e83fa
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-27 20:00:28 +0300

    Time: 2244 ms (8.33%), Space: 28.7 MB (75.00%) - LeetHub

---
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def numberOfPairs(self, nums1, nums2, diff):
+        self.diff = diff
+        self.pairs = 0
+        nums = self.arrayGenerate(nums1, nums2)
+        print(nums)
+        self.divide(nums)
+        return self.pairs
+        
+    def divide(self, array):
+        if len(array) < 2:
+             return array
+
+        left = self.divide(array[:len(array)//2])
+        right = self.divide(array[len(array)//2:])
+
+        return self.merger(left, right)
+        
+    def merger(self, left, right):
+        idx2 = 0
+        for idx1, num in enumerate(left):
+            while idx2 < len(right) and num - right[idx2] > self.diff:
+                idx2 += 1
+            self.pairs += len(right) - idx2
+
+        idx1 = idx2 =  0
+        merged = []
+        while idx1 < len(left) and idx2 < len(right):
+            if left[idx1] <= right[idx2]:
+                merged.append(left[idx1])
+                idx1 += 1
+            else:
+                merged.append(right[idx2])
+                idx2 += 1
+
+        merged.extend(left[idx1:])
+        merged.extend(right[idx2:])
+        
+        return merged
+    
+    def arrayGenerate(self, left, right):
+        new_array = []
+        for idx in range(len(left)):
+            new_array.append(left[idx]-right[idx])
+            idx += 1
+        return new_array
+            
+     
+            
+                
+            
+            
