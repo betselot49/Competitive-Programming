6533cd0cffe32885f1e4b19df177b8f03f215cfc
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-30 18:22:00 +0300

    Time: 87 ms (43.12%), Space: 16.8 MB (78.88%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        counter_arr = Counter(nums).items()
+        N = len(counter_arr)
+        print(counter_arr)
+        def quickSort(pivot, end, array, target):
+            read = write = pivot + 1
+            while read < end:
+                if array[pivot][1] >= array[read][1]:
+                    array[write], array[read] = array[read], array[write]
+                    write += 1
+                read += 1
+            array[write-1], array[pivot] = array[pivot], array[write-1]
+            if 0 <= len(counter_arr) - target - (write - 1) <= 1: return
+            if len(counter_arr) - target < write - 1:
+                quickSort(pivot, write-1, array, target)
+            else:
+                quickSort(write, end, array, target)
+            
+        quickSort(0, N, counter_arr, k)
+        return [i[0] for i in counter_arr[N-k:]]
+
+        
