a08c45a6b46f716e3e1f514079ced1036fab7611
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-03-30 18:33:17 +0300

    Time: 15 ms (81.11%), Space: 13.5 MB (11.11%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findComplement(self, num):
+        mask, limit = 1, num
+        while mask <= limit:
+            num ^= mask
+            mask <<= 1
+        return num
+        
