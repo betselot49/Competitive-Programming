303745af424adbff79afac49b414486a4553fd5e
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-04-04 17:51:20 +0300

    Time: 54 ms (5.83%), Space: 14.1 MB (16.97%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,44 @@
+# Using bit manipulation (turnign on or of the bit (idx) we already used) => instead of set using bit saves space (O(1)) 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        self.seen = set()
+        self.bit = 0
         self.permutation = []
-        def helper(nums, path):
+        def helper(N, nums, path):
             if len(path) == len(nums):
                 self.permutation.append(path)
                 return
             
-            for num in nums:
-                if num not in self.seen:
+            for idx, num in enumerate(nums):
+                if 2**(N-idx) & self.bit == 0:
                     path.append(num)
-                    self.seen.add(num)
-                    helper(nums, path[:])
+                    self.bit += 2 ** (N - idx)
+                    helper(N, nums, path[:])
                     path.pop()
-                    self.seen.remove(num)
-        helper(nums, [])
+                    self.bit -= 2 ** (N - idx)
+        
+        helper(len(nums)-1, nums, [])
         return self.permutation
+
+# Using set to avoid using numbers we already used. 
+
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         self.seen = set()
+#         self.permutation = []
+#         def helper(nums, path):
+#             if len(path) == len(nums):
+#                 self.permutation.append(path)
+#                 return
+            
+#             for num in nums:
+#                 if num not in self.seen:
+#                     path.append(num)
+#                     self.seen.add(num)
+#                     helper(nums, path[:])
+#                     path.pop()
+#                     self.seen.remove(num)
+        
+#         helper(nums, [])
+#         return self.permutation
+    
+    
