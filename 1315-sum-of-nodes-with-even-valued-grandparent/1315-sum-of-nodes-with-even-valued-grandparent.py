b5f45dd2efa274b3ac8ac3fc959e960864357863
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-04-17 20:10:12 +0300

    Time: 109 ms (56.93%), Space: 17.8 MB (48.00%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -7,18 +7,32 @@
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         self.sum = 0
-        def dfs(node):
+        def dfs(node, parent, grandparent):
             if not node: return 
-            if node.val % 2 == 0:
-                if node.left and node.left.left:
-                    self.sum += node.left.left.val
-                if node.left and node.left.right:
-                    self.sum += node.left.right.val
-                if node.right and node.right.left:
-                    self.sum += node.right.left.val
-                if node.right and node.right.right:
-                    self.sum += node.right.right.val
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
+            if grandparent % 2 == 0:
+                self.sum += node.val
+            dfs(node.left, node.val, parent)
+            dfs(node.right, node.val, parent)
+        dfs(root, 1, 1)
         return self.sum
+        
+        
+        
+        #  < --------------  Bruteforce   ---------------- >   
+        
+    
+        # def dfs(node):
+        #     if not node: return 
+        #     if node.val % 2 == 0:
+        #         if node.left and node.left.left:
+        #             self.sum += node.left.left.val
+        #         if node.left and node.left.right:
+        #             self.sum += node.left.right.val
+        #         if node.right and node.right.left:
+        #             self.sum += node.right.left.val
+        #         if node.right and node.right.right:
+        #             self.sum += node.right.right.val
+        #     dfs(node.left)
+        #     dfs(node.right)
+        # dfs(root)
+        # return self.sum
