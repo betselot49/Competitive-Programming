6166171b3c8c487534ef9969ef6661cb336a3a77
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-04-25 13:48:23 +0300

    Update 2447-number-of-subarrays-with-gcd-equal-to-k.py

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
@@ -4,30 +4,37 @@ class Solution:
         for left, num in enumerate(nums):
             right = left
             count = 0
-            while right < len(nums) and math.gcd(num, nums[right]) != k:
+            # find two numbers that satisfy the condition but the numbers between them are divisble by k
+            while right < len(nums) and math.gcd(num, nums[right]) != k: 
                 if nums[right] % k == 0:
                     right += 1
                 else:
                     right = len(nums)
             
+            # if there are no such two numbers fulfilling the condition => continue
             if right == len(nums):
                 continue
             
             count += 1
             right += 1
+            # open our window to the right until we get number not divisible by k or right being out of bound
             while right < len(nums) and nums[right] % k == 0:
                 right += 1
                 count += 1
-                
+             
+            
+            # open window to the left until we get number not divisible by k or left being out of bound 
             temp = left - 1
             leftCounter = 1
             while temp >= 0 and nums[temp] and nums[temp] % k == 0:
                 temp -= 1
                 leftCounter += 1
             
+            # calculate total subarrays that can be formed left window length * right window length
             count *= leftCounter
             nums[left] = 0
             sub_arrays += count
+            
         return sub_arrays
     
  
