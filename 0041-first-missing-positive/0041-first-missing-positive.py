759a718f93b3f35c121e348cbfeeb55f1872b968
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-05-06 16:19:02 +0300

    Time: 347 ms (85.04%), Space: 34.7 MB (8.63%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,23 +1,23 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # nums = set(nums)
-        # for i in range(1, len(nums)+1):
-        #     if i not in nums: return i
-        # return len(nums) + 1
-        
-        
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                nums[i] = 0
-                
-        for i in range(len(nums)):
-            num = abs(nums[i])
-            if 1 <= num <= len(nums):
-                if nums[num-1] > 0:
-                    nums[num - 1] *= -1
-                elif nums[num - 1] == 0:
-                    nums[num - 1] = -(len(nums)+1)
-        
+        nums = set(nums)
         for i in range(1, len(nums)+1):
-            if nums[i - 1] >= 0: return i
+            if i not in nums: return i
         return len(nums) + 1
+        
+        
+#         for i in range(len(nums)):
+#             if nums[i] < 0:
+#                 nums[i] = 0
+                
+#         for i in range(len(nums)):
+#             num = abs(nums[i])
+#             if 1 <= num <= len(nums):
+#                 if nums[num-1] > 0:
+#                     nums[num - 1] *= -1
+#                 elif nums[num - 1] == 0:
+#                     nums[num - 1] = -(len(nums)+1)
+        
+#         for i in range(1, len(nums)+1):
+#             if nums[i - 1] >= 0: return i
+#         return len(nums) + 1
