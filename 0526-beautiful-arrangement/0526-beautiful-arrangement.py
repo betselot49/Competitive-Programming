efbadb6d113d90bfcf031be4441e89035786e27c
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-05-07 21:42:06 +0300

    Time: 5226 ms (5.18%), Space: 16.4 MB (15.63%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,20 +1,20 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
         self.count = 0
-        visited = set()
         path = []
+        self.mask = 0
         def backtrack(idx, n):
             if idx > n:
                 self.count += 1
                 return
             
             for i in range(1, n+1):
-                if i not in visited and (i % idx == 0 or idx % i == 0):
+                if self.mask & 2 ** (i - 1) == 0 and (i % idx == 0 or idx % i == 0):
                     path.append(i)
-                    visited.add(i)
+                    self.mask ^= 2 ** (i - 1)
                     backtrack(idx+1, n)
+                    self.mask ^= 2 ** (i - 1)
                     path.pop()
-                    visited.discard(i)
         
         backtrack(1, n)
         return self.count
