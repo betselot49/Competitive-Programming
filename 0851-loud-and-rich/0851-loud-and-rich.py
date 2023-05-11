7217ad63c933dd8874c0f5932e1ff43244188efd
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-05-11 19:13:47 +0300

    Time: 491 ms (19.74%), Space: 26.3 MB (8.33%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.py
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -6,12 +6,15 @@ class Solution:
             graph[node1].append(node2)
             indegree[node2] += 1
         
+        # initially number itself could be answer
         answer = [i for i in range(len(quiet))]
+        
         queue = deque([i for i in range(len(indegree)) if not indegree[i]])
         quiet_value = { quietness: ind for ind, quietness in enumerate(quiet) }
         while queue:
             node = queue.popleft()
             for neighbour in graph[node]:
+                # update answer[neighbour] with minimun quiet_value of current value and the node's quiet value
                 answer[neighbour] = quiet_value[min(quiet[answer[neighbour]], quiet[answer[node]])]
                 indegree[neighbour] -= 1
                 if not indegree[neighbour]:
