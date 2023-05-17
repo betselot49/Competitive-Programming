9a55d41c3214a76ff37041a639e3c3bcc84d36c4
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-05-17 15:44:45 +0300

    Time: 675 ms (15.16%), Space: 18.2 MB (14.08%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,15 +1,19 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
+        # holds the next numbers needed for each group
         next_number = defaultdict(list)
         for num in nums:
             if not next_number[num]:
+                # the current number now needs a number greater than it self by one
                 heappush(next_number[num+1], (1, [num]))
             else:
+                # the number got its place so append it and add to the next_number on num+1 key
                 length, new_member = heappop(next_number[num])
                 new_member.append(num)
                 heappush(next_number[num+1], (length+1, new_member))
     
         for value in list(next_number.values()):
+            # check each group has more than or equal to 3 members
             for length, array in value:
                 if length < 3: return False
         return True
