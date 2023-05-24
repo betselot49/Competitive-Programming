98410f39ae2665def83c6084300366c5a9728ee3
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-05-24 18:02:54 +0300

    Time: 56 ms (5.34%), Space: 16.4 MB (5.34%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
@@ -0,0 +1,5 @@
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if k < numOnes: return k
+        if k < numOnes + numZeros: return numOnes
+        else: return numOnes - (k - (numOnes + numZeros))
