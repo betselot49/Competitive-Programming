9e4260111553f3116c54c71d7b54fde7b1f1341e
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-06-07 22:04:23 +0300

    Time: 46 ms (57.94%), Space: 16.2 MB (53.34%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def myAtoi(self, s):
+        MIN, MAX = -2 ** 31, 2 ** 31 - 1
+        n, empty, sign = 0, True, 1
+        for c in s:
+            if empty:
+                if c == ' ': continue 
+                elif c == '-': sign = -1 
+                elif c.isdigit(): n = int(c)  
+                elif c != '+': return 0  
+                empty = False 
+            else:
+                if c.isdigit():
+                    n = n * 10 + int(c)
+                    if sign * n > MAX: return MAX
+                    elif sign * n < MIN: return MIN
+                else: break   
+        return sign * n 
