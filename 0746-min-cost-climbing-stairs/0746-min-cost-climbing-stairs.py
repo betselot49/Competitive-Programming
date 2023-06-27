c76ee78e625ca61a5c0bf97cb91d7e6eb0cc1a17
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-06-27 12:13:05 +0300

    Time: 85 ms (8.06%), Space: 16.5 MB (31.14%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,14 +1,25 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {}
-        def backtrack(idx):
-            if idx >= len(cost): return 0
-            if idx in memo: return memo[idx]
+#         Top down
+#         memo = {}
+#         def backtrack(idx):
+#             if idx >= len(cost): return 0
+#             if idx in memo: return memo[idx]
             
-            memo[idx] = min(cost[idx] + backtrack(idx+1), cost[idx] + backtrack(idx+2))
-            return memo[idx]
+#             memo[idx] = min(cost[idx] + backtrack(idx+1), cost[idx] + backtrack(idx+2))
+#             return memo[idx]
         
-        backtrack(0)
-        return min(memo[0], memo[1])
+#         backtrack(0)
+#         return min(memo[0], memo[1])
+    
+    
+        #  bottom up
+        n = len(cost) 
+        memo = [0] * (n + 1)
+        for i in range(2, n+1):
+            memo[i] = min(memo[i-1] + cost[i-1], memo[i-2]+ cost[i-2])
+        return memo[n]
+    
+    
       
         
