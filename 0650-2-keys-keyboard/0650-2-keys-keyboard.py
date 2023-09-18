36c0d5df3a72386e74b199502596552959c59142
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-09-18 16:12:27 +0300

    Time: 45 ms (76.40%), Space: 16.1 MB (94.57%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,14 +1,18 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        self.count = 0
-        def primeFactor(init, n):
-            if n == 1: return self.count
-            for i in range(init, (n // 2) + 1):
-                if n % i == 0:
-                    n //= i
-                    self.count += i
-                    return primeFactor(i, n)
-            self.count += n
-            return primeFactor(n, 1)
-                           
+        self.total = 0
+        def primeFactor(base, num):
+            if num == 1: return self.total
+            
+            for i in range(base, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    self.total += i
+                    return primeFactor(2, num//i)
+                    
+            self.total += num
+            return primeFactor(num, 1)
+        
         return primeFactor(2, n)
+
+                    
+            
