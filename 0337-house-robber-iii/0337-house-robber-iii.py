947a7158ae1d7cf2000bf6c5ab8ac0ce48e825ff
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-09-18 15:42:08 +0300

    Time: 42 ms (96.70%), Space: 18.5 MB (77.87%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -1,11 +1,17 @@
-class Solution(object):
-    def rob(self, root):
-        def superrob(node):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
             if not node: return (0, 0)
-            left, right = superrob(node.left), superrob(node.right)
-            now = node.val + left[1] + right[1]
-            later = max(left) + max(right)
+            left, right = dfs(node.left), dfs(node.right)
+            take = node.val + left[1] + right[1]
+            skip = max(left) + max(right)
             
-            return (now, later)
-            
-        return max(superrob(root))
+            return (take, skip)
+        
+        return max(dfs(root))
