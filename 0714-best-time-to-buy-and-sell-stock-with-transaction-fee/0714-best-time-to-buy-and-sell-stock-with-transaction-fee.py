25a83c557f71c983db05a913e4549dad3c8dc688
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-09-18 12:00:33 +0300

    Time: 719 ms (38.32%), Space: 30.9 MB (23.78%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @cache
-        def dp(idx, turn):
-            if idx == len(prices):
-                return 0
-            if turn == "SELL":
-                return max(dp(idx + 1,"BUY") + prices[idx], dp(idx + 1, "SELL"))
-            else:
-                return max(dp(idx + 1, "SELL") - prices[idx] - fee, dp(idx + 1, "BUY"))
-
-        return dp(0, 0)
+        dp, N = defaultdict(lambda : (0, 0)), len(prices)
+        for i in range(N-1, -1, -1):
+            # buying at this index
+            buy = dp[i+1][1] - prices[i]          
+            buy = max(buy, dp[i+1][0])
+            
+            # selling at this index
+            sell = dp[i+1][0] + (prices[i] - fee)
+            sell = max(sell, dp[i+1][1])
+            
+            # store dp[i] in the format of (max_buy, max_sell)
+            dp[i] = (buy, sell)
+        
+        return dp[0][0]
