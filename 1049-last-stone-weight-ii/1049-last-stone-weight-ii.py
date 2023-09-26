e6aa47c7f5563e0d0ea2c5a97fc420a560ff1284
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-09-26 15:27:02 +0300

    Time: 96 ms (9.07%), Space: 25.9 MB (5.24%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,7 +1,17 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         @cache
-        def balanced_diff(G1, G2, i):
-            # min ( add to G1, add to G2 ) 
-            return abs(G2 - G1) if i >= len(stones) else min(balanced_diff(G1 + stones[i], G2, i + 1), balanced_diff(G1, G2 + stones[i], i + 1))
+        def balanced_diff(group1, group2, idx):
+            if idx >= len(stones):
+                return abs(group2 - group1)
+            
+            add_to_G1 = balanced_diff(group1 + stones[idx], group2, idx + 1)
+            add_to_G2 = balanced_diff(group1, group2  + stones[idx], idx + 1)
+
+            return min(add_to_G1, add_to_G2)
+        
         return balanced_diff(0, 0, 0)
+    
+    
+    # One liner
+    def backtrack(G1, G2, i): return abs(G2 - G1) if i >= len(stones) else min(backtrack(G1 + stones[i], G2, i + 1), backtrack(G1, G2 + stones[i], i + 1))
