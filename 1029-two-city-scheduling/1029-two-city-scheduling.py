7b78815de02ca60d91c56d9f58e9b3a0fc143442
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-03 14:55:11 +0300

    Time: 43 ms (79.65%), Space: 16 MB (99.96%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,10 +1,7 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # sort based on the cost differnce to go to B rather than A
-        costs.sort(key=lambda x : x[0] - x[1])
-        n = len(costs)
-        running_sum = 0
-        for i in range(n//2):
-            running_sum += costs[i][0]
-            running_sum += costs[n-1-i][1]
-        return running_sum
+        costs.sort(key = lambda x : x[0] - x[1])
+        min_cost, n = 0, len(costs)
+        for i in range(n // 2):
+            min_cost += costs[i][0] + costs[n-i-1][1]
+        return min_cost
