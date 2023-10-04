9d7fc7c4e957ca980d326635347ac6395b55ea08
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-04 11:16:55 +0300

    Time: 636 ms (82.11%), Space: 20.3 MB (41.62%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,35 +1,32 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = defaultdict(lambda : set())
-        indegree = defaultdict(int)
+        graph = defaultdict(set)
+        indegree = [0] * numCourses
         for course1, course2 in prerequisites:
             graph[course2].add(course1)
             indegree[course1] += 1
             
-        prereq_set = [set() for _ in range(numCourses)]
+        prerequisite_set = [set() for _ in range(numCourses)]
         queue = deque([])
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+        
+        for node in range(numCourses):
+            if indegree[node] == 0:
+                queue.append(node)
                 
         while queue:
-            curr = queue.popleft()
-            for neighbour in graph[curr]:
-                prereq_set[neighbour].update(prereq_set[curr])
-                prereq_set[neighbour].add(curr)
+            node = queue.popleft()
+            for neighbour in graph[node]:
+                prerequisite_set[neighbour].update(prerequisite_set[node])
+                prerequisite_set[neighbour].add(node)
                 indegree[neighbour] -= 1
                 if indegree[neighbour] == 0:
                     queue.append(neighbour)
-        
-        answer = []
-        for course1, course2 in queries:
-            if course2 in prereq_set[course1]:
-                answer.append(True)
+                    
+        thruth_val = []
+        for c1, c2 in queries:
+            if c2 in prerequisite_set[c1]:
+                thruth_val.append(True)
             else:
-                answer.append(False)
+                thruth_val.append(False)
                 
-        return answer
-                
-        
-            
-       
+        return thruth_val
