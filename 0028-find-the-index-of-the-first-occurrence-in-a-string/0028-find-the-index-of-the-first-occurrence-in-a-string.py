c8bb8358efc06d286b9f91fedc52af0ce0acd127
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-09 10:52:52 +0300

    Time: 38 ms (57.24%), Space: 16.2 MB (82.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,13 +1,42 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        hash = {}
         m = len(needle)
+        print(len(haystack))
+        
+        start = haystack[:m]
+        start = start[::-1]
+     
+     
+        hash_map = {}
+        
+        def preCompute(word):
+            alpha = 27
+            num = 0
+            for i, char in enumerate(word):
+                num += (ord(char) - 96) * (alpha ** i)
+            return num
+        
+        def add(word, char):
+            word *= 27
+            word += ord(char) - 96
+            return word
+        
+        def remove(word, char, m):
+            word -= (ord(char) - 96) * (27 ** m)
+            return word
+            
+            
+        first = preCompute(haystack[:m][::-1])
+        needle = preCompute(needle[::-1]) 
+        hash_map[0] = first
+        for i in range(0, len(haystack) - m):
+            curr = remove(first, haystack[i], m-1)
+            curr = add(curr, haystack[i+m])
+            hash_map[i+1] = curr
+            first = curr
+            
         for i in range(len(haystack) - m + 1):
-            temp = i
-            flag = True
-            for j in range(m):
-                if haystack[temp] != needle[j]:
-                    flag = False
-                    break
-                temp += 1
-            if flag: return i
+            if hash_map[i] == needle: return i
         return -1
+          
