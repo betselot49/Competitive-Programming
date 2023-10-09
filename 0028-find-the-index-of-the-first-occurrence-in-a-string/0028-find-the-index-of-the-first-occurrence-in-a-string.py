aa25f5cd3ba5045be9cc611f8c2fb5c80c9bfa48
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-09 10:57:37 +0300

    Time: 45 ms (13.33%), Space: 16.3 MB (18.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,17 +3,20 @@ class Solution:
         m = len(needle)
         hash_map = {}
         
+        # precompure needle and substring in hystack with length needle
         def preCompute(word):
             num = 0
             for i, char in enumerate(word):
                 num += (ord(char) - 96) * (27 ** i)
             return num
         
+        # add to the right if the string => word is a number representation not a string
         def add(word, char):
             word *= 27
             word += ord(char) - 96
             return word
         
+        # remove from the left
         def remove(word, char, m):
             word -= (ord(char) - 96) * (27 ** m)
             return word
@@ -23,6 +26,7 @@ class Solution:
         needle = preCompute(needle[::-1]) 
         
         hash_map[0] = first
+        # find the maching substring
         for i in range(0, len(haystack) - m):
             curr = remove(first, haystack[i], m-1)
             curr = add(curr, haystack[i+m])
