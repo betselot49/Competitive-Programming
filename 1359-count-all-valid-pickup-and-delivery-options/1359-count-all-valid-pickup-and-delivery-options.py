5c017af2cad36ff565b0f61b81dafc2d792ab346
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-12 18:21:28 +0300

    Time: 34 ms (83.71%), Space: 16.2 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,7 +1,9 @@
 class Solution:
     def countOrders(self, n: int) -> int:
+        MOD = (10 ** 9)+ 7   
+        return (factorial(2*n) // 2 ** n) % MOD
+    
         dp = [0, 1]
-        MOD = (10 ** 9)+ 7
         for i in range(2, n+1):
             hole = ((i - 1) * 2) + 1
             total = ((1 + hole) * hole) // 2
