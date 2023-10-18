3ab699fc7ed0001bb4f50e081cefbd98f3e22cb8
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-18 16:07:28 +0300

    Time: 38 ms (58.24%), Space: 16.3 MB (19.89%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,24 +1,16 @@
 class Solution:
     def __init__(self):
-        self.cache = {}
-        
+        self.cache = {0 : 1}
+      
     def power(self, x, n):
-        if n == 0:
-            return 1
+        if n in self.cache: return self.cache[n]
+
+        if n == 1: return x
         
-        if n in self.cache:
-            return self.cache[n]
-        if n % 2 == 0:
-            power1 = self.power(x, n // 2)
-            power2 = self.power(x, n // 2) 
-            
-            self.cache[n] = power1 * power2
-            return self.cache[n]
-        else:
-            power = self.power(x, n-1) 
-            self.cache[n] = x * power
-            
-            return self.cache[n] 
+        power = self.power(x, n//2)
+        power *= power
+        
+        return power if n % 2 == 0 else power * x
     
     
     def myPow(self, x: float, n: int) -> float:
