97dacf37656d771cf33e1b8e5128c1b55bb2558d
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-18 15:03:28 +0300

    Time: 44 ms (24.34%), Space: 16.1 MB (78.29%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,4 +1,4 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = (10 ** 9) + 7
-        return (pow(5, ceil(n/2), MOD) * pow(4, n//2, MOD)) % MOD
+        return (pow(5, n - n // 2, MOD) * pow(4, n//2, MOD)) % MOD
