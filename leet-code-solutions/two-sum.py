04ca8c5e3f4ff9a5fcbb41618ebaf7d79e5030b9
Betselot Kidane <107115522+betselot49@users.noreply.github.com>
2023-10-29 21:08:39 +0300

    Added solution for Two Sum

--- leet-code-solutions/two-sum.py
+++ leet-code-solutions/two-sum.py
@@ -1,12 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        index = []
-        for i in range(len(nums)):
-            for j in range (i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    index.append(i)
-                    index.append(j)
-                    break
-                    
-        return index
-                    
+        # Follow-up --> O(N) 
+        look_back = {}
+        for idx, num in enumerate(nums):
+            if num in look_back: return [idx, look_back[num]]
+            look_back[target - num] = idx
